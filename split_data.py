6e50aaa76564ebccbba5afdaa2b50f72d1f923fa
张志杰 <50226404+Jasonx001@users.noreply.github.com>
2021-07-29 19:52:37 +0800

    Update split_data.py

--- split_data.py
+++ split_data.py
@@ -1,19 +1,21 @@
 import os
 import random
 
+# This script can split your dataset as train--val--test subset
 
-files_path = "G:\\研究生\\数据集\\VOC2012\\Annotations" #windows下路径写法
+files_path = "G:\\VOC2012\\Annotations" #This is a annotation path of your dataset.
 if not os.path.exists(files_path):
     print("文件夹不存在！")
     exit(1)
-test_rate = 0.2
-val_rate = 0.2
+test_rate = 0.2 #the test_rate is the ratio of test set over your whole dataset.
+val_rate = 0.2 #the val_rate is the ratio of val set over your trainval subset.
 
 files_name = sorted([file.split(".")[0] for file in os.listdir(files_path)])
 files_num = len(files_name)
 test_index = random.sample(range(0, files_num), k=int(files_num*test_rate))
 trainval_files = []
 test_files = []
+# The first loop split the dataset as test and trainval subset.
 for index, file_name in enumerate(files_name):
     if index in test_index:
         test_files.append(file_name)
@@ -24,6 +26,8 @@ trainval_num = len(trainval_files)
 val_index = random.sample(range(0, trainval_num), k = int(trainval_num * val_rate))
 train_files = [];
 val_files = [];
+# the second loop split the trainval sebset as train and val subset.
+# the number of files in val set accounts 0.2 of the trainval subset. 
 for index, file_name in enumerate(trainval_files):
     if index in val_index:
         val_files.append(file_name)
